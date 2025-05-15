58c4e5b96f201c3458cf200271d7ecb81f93d485
Michael Moffet <nanomoffet@gmail.com>
2025-05-15 16:38:51 -0700

    update to dotfiles 2025-05-15 16:38:51

--- .config/sketchybar/settings.lua
+++ .config/sketchybar/settings.lua
@@ -2,7 +2,7 @@ return {
 	paddings = 3,
 	group_paddings = 5,
 
-	icons = "NerdFont", -- alternatively available: NerdFont
+	icons = "sf-symbols", -- alternatively available: NerdFont
 
 	-- This is a font configuration for SF Pro and SF Mono (installed manually)
 	font = require("helpers.default_font"),
