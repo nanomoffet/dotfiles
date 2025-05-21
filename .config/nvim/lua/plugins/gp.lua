bdbb849b424c86a42bfd53d8b0142efa708da3cb
Michael Moffet <nanomoffet@gmail.com>
2025-05-20 20:11:06 -0700

    update to dotfiles 2025-05-20 20:11:06

--- .config/nvim/lua/plugins/gp.lua
+++ .config/nvim/lua/plugins/gp.lua
@@ -7,7 +7,7 @@ return {
         googleai = {
           endpoint = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-pro-preview-05-06:streamGenerateContent?key={{secret}}",
           model = "",
-          secret = { "bws", "secret", "get", "BWS_SECRETS_KEY", "--access-token", "BWS_ACCESS_TOKEN" },
+          secret = { "bws secret get BWS_SECRETS_KEY --access-token BWS_ACCESS_TOKEN --output env" },
         },
       },
     }
